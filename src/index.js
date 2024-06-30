3f3667519e9459d4c78c764a6d7f856900602647
Cy Logan <clogan2004@gmail.com>
2024-06-30 17:19:24 -0400

    Redux added and used

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import { createRoot } from "react-dom/client";
+import { Provider} from 'react-redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { createLogger } from 'redux-logger';
+import { thunk } from 'redux-thunk';
 import './index.css';
 import App from './containers/App';
 import 'tachyons';
+import { searchRobots, requestRobots } from './reducers';
+
+const logger = createLogger();
+const rootReducer = combineReducers({searchRobots, requestRobots});
+const store = createStore(rootReducer, applyMiddleware(thunk, logger));
 
 const root = createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+    <Provider store={store}>
+        <App />
+    </Provider>);
